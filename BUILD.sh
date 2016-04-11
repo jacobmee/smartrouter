fc7f301296d31fc10be8fd402db23b8c7139f8f9
Jacob Mee <jacob@mitang.me>
2016-04-11 16:06:24 +0800

    SyncY solution to enable Baidu Pan

--- BUILD.sh
+++ BUILD.sh
@@ -20,10 +20,15 @@ cp -r packages/* $BUILD_DEST/packages/
 
 echo "# Run the ImageBuilder"
 cd $BUILD_DEST
-make image PROFILE=TLWDR4300 PACKAGES="luci luci-app-firewall luci-i18n-english luci-lib-ipkg luci-lib-sys luci-proto-core luci-sgi-cgi luci-theme-base luci-theme-openwrt ip vpnc msmtp kmod-fs-nfs kmod-fs-nfs-common nfs-utils vsftpd openssh-sftp-server" FILES=files/
 
-# Option #1. Baidu Pan
-# kmod-nls-utf8 python python-curl SyncY-Python-luci
+# This has included the SyncY solution to enable the Baidu sync
+# Couple issues with this solution
+# 1) Python takes 4M spaces
+# 2) Performance could be worse
+# 3) So far there are one issue are not resolved yet.
+
+make image PROFILE=TLWDR4300 PACKAGES="luci luci-app-firewall luci-i18n-english luci-lib-ipkg luci-lib-sys luci-proto-core luci-sgi-cgi luci-theme-base luci-theme-openwrt ip vpnc msmtp kmod-fs-nfs kmod-fs-nfs-common nfs-utils vsftpd openssh-sftp-server kmod-nls-utf8 python python-curl SyncY-Python-luci" FILES=files/
+
 
 echo "# Copy the target file into this Bin files"
 cd $DEST
