6081c497b9ebe31f5d53e7e8fc0a57119eca6aa5
Jacob Mee <jacob@mitang.me>
2016-04-25 16:24:24 +0800

    Improve the log messages

--- src/etc/smartrouter/checkpoint.sh
+++ src/etc/smartrouter/checkpoint.sh
@@ -36,32 +36,31 @@ then
                 then 
 			let ENABLED_SS=0 # Reset VPN count
 			echo $ENABLED_SS>/etc/smartrouter/ENABLED_SS
-			print_log "[SHADOWSOCKS]: NEW Connected. ID: #("$PID_F"),#("$ENABLED_SS")" true "enabled.mail"
+			print_log "[SHADOWSOCKS]: NEW Connected. #ID: ("$PID_F"),("$ENABLED_SS")" true "enabled.mail"
                	else
-			# Logger every #30 minutes
-			#LOGMINUTE=$(date +"%M")
-			#if [ $(($LOGMINUTE%5)) == "0" ]
-			#then
-				print_log "[SHADOWSOCKS]: Connnected as always. ID: ("$PID_F"),#("$ENABLED_SS")"
-			#fi
+			# Logger every #10 minutes
+			LOGMINUTE=$(date +"%M")
+			if [ "$(($LOGMINUTE%10))" == "0" ]
+			then
+				print_log "[SHADOWSOCKS]: Connnected as usual. #ID: ("$PID_F"),("$ENABLED_SS")"
+			fi
 		fi
 
 	else
-		print_log "[SHADOWSOCKS]: Alives but RETURNCODE: #["$RETURNCODE"], ID: #("$PID_F"),#("$ENABLED_SS")" true
+		print_log "[SHADOWSOCKS]: Alive but returned #"$RETURNCODE". #ID: ("$PID_F"),("$ENABLED_SS")" true
 		# /etc/init.d/shadowsocks stop
 	fi
 
-	# Return successfull
 	return 0
 else
 	# It means the Shadowsocks for some reason is just gone.
-	print_log "[SHADOWSOCKS]: The service is not started, #("$ENABLED_SS")." true
+	print_log "[SHADOWSOCKS]: The service is not started. #("$ENABLED_SS")." true
 fi
 
 # if it was good last time, notify me the SS is disabled.
 if [ $ENABLED_SS -eq 0 ]
 then
-        print_log "[SHADOWSOCKS]: The service sas good last time,#("$ENABLED_SS")." true "disabled.mail" 
+        print_log "[SHADOWSOCKS]: The service sas good last time. #("$ENABLED_SS")." true "disabled.mail" 
 fi
 
 # Error+1
@@ -71,16 +70,18 @@ echo $ENABLED_SS>/etc/smartrouter/ENABLED_SS
 # Restart DNSMASQ
 # The reason why it needs to add the google DNS before DNSMASQ is because
 # Maybe DNS is incorrect, so the adresses are resolved will lead to wrong address.
+
+# Disable the refresh because looks uneccessary
 # /etc/init.d/dnsmasq restart
 # logger -s " >>>>> DNSMASQ REFRESHED <<<<< "
 	
 # Start Shadowsocks
 /etc/init.d/shadowsocks start
 sleep 10
-print_log "[Shadowsocks] >>>>>>>> Started <<<<<<<<<< "
-
 PID_F=`pgrep -f "ss-tunnel"`
-	
+print_log "[SHADOWSOCKS]: The service is started. #ID: ("$PID_F"),("$ENABLED_SS")"
+
+
 # If restart Shadowsocks successful
 if [ $PID_F ] && [ $PID_F -gt 0 ]
 then
@@ -88,7 +89,7 @@ then
 	wget --spider --quiet --tries=3 --timeout=3 www.google.com
 	if [ "$?" == "0" ]
 	then
-		print_log "[SHADOWSOCKS]: NEW Connected. ID: #("$PID_F"),#("$ENABLED_SS")" true "enabled.mail"
+		print_log "[SHADOWSOCKS]: NEW Connected. #ID: ("$PID_F"),("$ENABLED_SS")" true "enabled.mail"
 			
 		if [ $ENABLED_SS -gt 0 ]  
                 then 
@@ -101,12 +102,12 @@ then
 		wget --spider --quiet --tries=3 --timeout=3 www.baidu.com
 		if [ "$?" == "0" ]
 		then
-			print_log "[SHADOWSOCKS]: Problems still exsits after restarting the service. PID:#("$PID_F"),#("$ENABLED_SS")" true
+			print_log "[SHADOWSOCKS]: Problems still exsits after restarting the service. #ID: ("$PID_F"),("$ENABLED_SS")" true
 		else
-			print_log "[SHADOWSOCKS]: Network issue after restarting the service, so rebooting. PID:#("$PID_F"),#("$ENABLED_SS")" true "rebooted.mail"
+			print_log "[SHADOWSOCKS]: Network issue after restarting the service, so rebooting. #ID: ("$PID_F"),("$ENABLED_SS")" true "rebooted.mail"
 			sh /etc/smartrouter/x-reboot.sh
 		fi
 	fi
 else 	# Shadowsocks can't be started, please check manually
-	print_log "[SHADOWSOCKS]:  Couldn't start the service,#("$ENABLED_SS") please check manually." true "alert.mail"
+	print_log "[SHADOWSOCKS]:  Couldn't start the service. #("$ENABLED_SS") please check manually." true "alert.mail"
 fi
