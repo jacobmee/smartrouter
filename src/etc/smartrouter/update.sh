5e501b002c0ddc2b442b1496622f8e35170a58f8
Jacob Mee <jacob@mitang.me>
2016-06-03 00:11:20 +0800

    Including the DNS update for Apple store

--- src/etc/smartrouter/update.sh
+++ src/etc/smartrouter/update.sh
@@ -4,6 +4,8 @@ wget --spider --quiet --tries=1 --timeout=3 www.mitang.me
 if  [ "$?" == "0" ]
 then
 	wget www.mitang.me/dnsmasq_list.conf -O /etc/smartrouter/dnsmasq_list.conf	
-	cp /etc/smartrouter/dnsmasq_list.conf /etc/dnsmasq.d/
-	rm /etc/smartrouter/dnsmasq_list.conf
+	mv /etc/smartrouter/dnsmasq_list.conf /etc/dnsmasq.d/
+
+	wget www.mitang.me/dnsmasq_apple.conf -O /etc/smartrouter/dnsmasq_apple.conf
+	mv /etc/smartrouter/dnsmasq_apple.conf /etc/dnsmasq.d/
 fi
