5c7c98b64868f0f1d2681023898b7f317f9d761d
Jacob Mee <jacob@mitang.me>
2016-06-03 22:16:05 +0800

    Update DHCP

--- src/etc/smartrouter/update.sh
+++ src/etc/smartrouter/update.sh
@@ -6,6 +6,6 @@ then
 	wget www.mitang.me/dnsmasq_list.conf -O /etc/smartrouter/dnsmasq_list.conf	
 	mv /etc/smartrouter/dnsmasq_list.conf /etc/dnsmasq.d/
 
-	wget www.mitang.me/dnsmasq_apple.conf -O /etc/smartrouter/dnsmasq_apple.conf
-	mv /etc/smartrouter/dnsmasq_apple.conf /etc/dnsmasq.d/
+	#wget www.mitang.me/dnsmasq_apple.conf -O /etc/smartrouter/dnsmasq_apple.conf
+	#mv /etc/smartrouter/dnsmasq_apple.conf /etc/dnsmasq.d/
 fi
