6a8349ce539246287b2a99094c429ce2b4d52b41
Jacob Mee <jacob@mitang.me>
2016-11-27 16:45:55 -0800

    Version for Lede

--- BUILD.sh
+++ BUILD.sh
@@ -2,7 +2,7 @@
 
 # Define the destination
 # BUILD_DEST=~/Workspace/OpenWrt-ImageBuilder-15.05.1-mvebu.Linux-x86_64
-BUILD_DEST=~/Workspace/Router.LinkSys.WRT1900ACS2/OpenWrt-ImageBuilder-15.05.1-mvebu.Linux-x86_64
+BUILD_DEST=~/Workspace/Router.LinkSys.WRT1900ACS2/lede-imagebuilder-mvebu.Linux-x86_64
 
 DEST=~/Workspace/git/repos/smartrouter
 
@@ -27,13 +27,13 @@ echo "# Run the ImageBuilder"
 # With 5M spaced needed.
 
 cd $BUILD_DEST
-make image PROFILE=Shelby PACKAGES="luci luci-app-firewall luci-theme-openwrt ip wget msmtp vsftpd openssh-sftp-server iptables-mod-nat-extra ipset libopenssl dnsmasq-full shadowsocks-libev -dnsmasq" FILES=files/
-
+make image PROFILE=linksys-wrt1900acs PACKAGES="luci luci-theme-material wget msmtp vsftpd openssh-sftp-server iptables-mod-nat-extra ipset libopenssl dnsmasq-full shadowsocks-libev" FILES=files/
+# dnsmasq-full shadowsocks-libev -dnsmasq
 
 echo "# Copy the target file into this Bin files"
 cd $DEST
 mkdir bin
-cp $BUILD_DEST/bin/mvebu/**shelby* bin/
+cp $BUILD_DEST/build_dir/target-arm_cortex-a9+vfpv3_musl-1.1.15_eabi/linux-mvebu/tmp/**wrt1900acs* bin/
 
 echo "# Clean files"
 rm -rf re$BUILD_DEST/files
