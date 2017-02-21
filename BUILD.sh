59151b36fc4b77f9740f057f6739660d8496af0d
Jacob Mee <jacob@mitang.me>
2017-02-21 05:59:48 -0800

    Build files for Lede-17.01.0

--- BUILD.sh
+++ BUILD.sh
@@ -2,7 +2,7 @@
 
 # Define the destination
 # BUILD_DEST=~/Workspace/OpenWrt-ImageBuilder-15.05.1-mvebu.Linux-x86_64
-BUILD_DEST=~/Workspace/Router.LinkSys.WRT1900ACS2/lede-imagebuilder-mvebu.Linux-x86_64
+BUILD_DEST=~/Workspace/Router.LinkSys.WRT1900ACS2/lede-imagebuilder-17.01.0-mvebu.Linux-x86_64
 
 DEST=~/Workspace/git/repos/smartrouter
 
@@ -33,7 +33,7 @@ make image PROFILE=linksys-wrt1900acs PACKAGES="luci luci-theme-material wget ms
 echo "# Copy the target file into this Bin files"
 cd $DEST
 mkdir bin
-cp $BUILD_DEST/build_dir/target-arm_cortex-a9+vfpv3_musl-1.1.15_eabi/linux-mvebu/tmp/**wrt1900acs* bin/
+cp $BUILD_DEST/build_dir/target-arm_cortex-a9+vfpv3_musl-1.1.16_eabi/linux-mvebu/tmp/**wrt1900acs* bin/
 
 echo "# Clean files"
 rm -rf re$BUILD_DEST/files
