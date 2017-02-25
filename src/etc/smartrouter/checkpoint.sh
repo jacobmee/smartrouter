ebd5693b7b91eee54585acf41016bbfb7652fb58
Jacob Mee <jacobmee@gmail.com>
2017-02-25 10:10:14 +0800

    Improving shadowsocks starting script

--- src/etc/smartrouter/checkpoint.sh
+++ src/etc/smartrouter/checkpoint.sh
@@ -12,20 +12,17 @@ ENABLED_SS=`sed -n 1p "/etc/smartrouter/ENABLED_SS"`
 print_log () {
 	logger -s $1
 	
-	if [ "$2" = true ]
-	then
+	if [ "$2" = true ]; then
 		echo "["$LOGTIME"] "$1 >> /etc/smartrouter/reconnect.log
 	fi
 
-	if [ "$3" ]
-	then
+	if [ "$3" ]; then
 		cat /etc/smartrouter/mails/$3 | msmtp -a default admin@mitang.me
 	fi
 }
 
 # DDNS heartbeats
-if [ "$(($LOGMINUTE%60))" == "0" ]
-then
+if [ "$(($LOGMINUTE%60))" == "0" ]; then
 	wget -O /etc/smartrouter/ddns.log http://jacobmee:jac0bm11@ddns.oray.com/ph/update?hostname=jacobmee.eicp.net
 	ddns=$(cat "/etc/smartrouter/ddns.log")
 
@@ -39,84 +36,75 @@ then
 fi
 
 # See Shadowsocks is already started, and work properly
-if [ $PID_F ] && [ $PID_F -gt 0 ]
-then
+if [ $PID_F ] && [ $PID_F -gt 0 ]; then
 	wget --spider --quiet --tries=3 --timeout=3 www.google.com
 	RETURNCODE=$?
-	if  [ "$RETURNCODE" == "0" ]
-	then
-		# This is first time it works again.
-		if [ $ENABLED_SS -gt 0 ]
-		then
-			let ENABLED_SS=0 # Reset VPN count
+
+	# This is first time it works again.
+	if  [ "$RETURNCODE" == "0" ]; then
+		# Reset Error counts
+		if [ $ENABLED_SS -gt 0 ]; then
+			let ENABLED_SS=0
 			echo $ENABLED_SS>/etc/smartrouter/ENABLED_SS
 			print_log "[SHADOWSOCKS]: Back to work. #ID: ("$PID_F"),("$ENABLED_SS")"
 		else
 			# Logger every #60 minutes
-			if [ "$(($LOGMINUTE%60))" == "0" ]
-			then
+			if [ "$(($LOGMINUTE%60))" == "0" ]; then
 				print_log "[SHADOWSOCKS]: Hourly checkpoint passed. #ID: ("$PID_F"),("$ENABLED_SS")"
-			#else
-				#print_log "[SHADOWSOCKS]: Checkpoint passed." false
 			fi
 		fi
 
 		# Checkpoint passed successfully.
 		return 0
-	else
-		# Error+1
-		let ENABLED_SS=$ENABLED_SS+1  # Set Shadowsocks count one more
-		echo $ENABLED_SS>/etc/smartrouter/ENABLED_SS
-
-		# 4 means "Network Failure"
-		if  [ "$RETURNCODE" == "4" ]
-		then
-			wget --spider --quiet --tries=3 --timeout=3 www.baidu.com
-			print_log "[SHADOWSOCKS]: I'm alive. However, Baidu gave me #"$?" & Google gave me #"$RETURNCODE". #ID: ("$PID_F"),("$ENABLED_SS")" true
-			# Unnecessary to restart the shadowsocks, maybe a random issue
-		else
-			print_log "[SHADOWSOCKS]: I'm alive, but Google gave me #"$RETURNCODE". #ID: ("$PID_F"),("$ENABLED_SS")" true
-			# This requires a restart.
-		fi
-
-		if  [ $ENABLED_SS -lt 4 ]
-		then
-			return 0
-		fi
 	fi
+
+	wget --spider --quiet --tries=3 --timeout=3 www.baidu.com
+	print_log "[SHADOWSOCKS]: I'm alive. However, Baidu gave me #"$?" & Google gave me #"$RETURNCODE". #ID: ("$PID_F"),("$ENABLED_SS")" true
+
 fi
 
-print_log "[SHADOWSOCKS & DNSMASQ]: The services are restarting... #E: ("$ENABLED_SS")"
-# Restart DNSMASQ for refresh
-/etc/init.d/dnsmasq restart
+# Error+1
+let ENABLED_SS=$ENABLED_SS+1  # Set Shadowsocks count one more
+echo $ENABLED_SS>/etc/smartrouter/ENABLED_SS
+
+# Don't necessary to restart if errors < 4 times
+if  [ $ENABLED_SS -lt 4 ]; then
+	return 0
+fi
+
+print_log "[SHADOWSOCKS & NETWORK]: The services are restarting... #E: ("$ENABLED_SS")"
+# Refresh the network
+/etc/init.d/network restart
+sleep 8
+
 # Start Shadowsocks
 /etc/init.d/shadowsocks-libev restart
-sleep 2
 
+# Double check if everything is fine
 PID_F=`pgrep -f "ss-redir"`
-# If restart Shadowsocks successful
-if [ $PID_F ] && [ $PID_F -gt 0 ]
-then
-	# Test Google
+if [ $PID_F ] && [ $PID_F -gt 0 ]; then
+
 	wget --spider --quiet --tries=3 --timeout=3 www.google.com
-	if [ "$?" == "0" ]
-	then
+	if [ "$?" == "0" ]; then
 		print_log "[SHADOWSOCKS]: Successfully NEW connected. #ID: ("$PID_F"),("$ENABLED_SS")" true "enabled.mail"
 			
-		if [ $ENABLED_SS -gt 0 ]
-		then
+		if [ $ENABLED_SS -gt 0 ]; then
 			let ENABLED_SS=0 # Reset VPN count
 			echo $ENABLED_SS>/etc/smartrouter/ENABLED_SS
 		fi
         
-	# Google is still failing, try to test Baidu
+	# Try Baidu
 	else
 		wget --spider --quiet --tries=3 --timeout=3 www.baidu.com
-		if [ "$?" == "0" ]
-		then
-			print_log "[SHADOWSOCKS]: Problems still exists after restarting the service. #ID: ("$PID_F"),("$ENABLED_SS")" true
+		if [ "$?" == "0" ]; then
+			print_log "[SHADOWSOCKS]: Still can't access Google after restarting the service. #ID: ("$PID_F"),("$ENABLED_SS")" true
 		else
-			print_log "[SHADOWSOCKS]: Network issue after restarting the service, so require reboot?. #ID: ("$PID_F"),("$ENABLED_SS")" true "alert.mail"
+			print_log "[SHADOWSOCKS]: Still everything is down after restarting the service. #ID: ("$PID_F"),("$ENABLED_SS")" true
+		fi
+
+		if  [ $ENABLED_SS -gt 60 ]; then
+			print_log "[SHADOWSOCKS]: Counting 60 (3 hours) already, rebooting the router. #ID: ("$PID_F"),("$ENABLED_SS")" true "rebooted.mail"
+			reboot
 		fi
 	fi
 else 	# Shadowsocks can't be started, please check manually
