65a209a5b4db7547562df2dbb18a75d5b7213d69
Jacob Mee <jacobmee@gmail.com>
2018-08-03 15:35:38 +0800

    Update BUILD.17.sh

--- BUILD.17.sh
+++ BUILD.17.sh
@@ -38,7 +38,7 @@ cp $BUILD_DEST/build_dir/target-arm_cortex-a9+vfpv3_musl-1.1.16_eabi/linux-mvebu
 
 echo "# Clean files"
 rm -rf re$BUILD_DEST/files
-rm -rf $BUILD_DEST/bin
+#rm -rf $BUILD_DEST/bin
 
 echo "##############################################"
 echo "#    DONE, Please check the BIN folder!      #"
