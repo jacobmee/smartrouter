eef05b100d148b7f0a751d2fbb1fb3cb570af0bb
Mi <jacobmee@gmail.com>
2020-05-06 23:22:42 +0800

    PPPoe & DHCP with pointing to a side router

--- src/etc/smartrouter/opkg-upgrade-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-for p in $(opkg list-upgradable); do
-  if [ "$p" != "-" ]; then
-    if [ "$(echo ${p:0:1} | sed -e 's/[0-9]//')" != "" ]; then
-      echo "upgrading $p"
-      opkg upgrade $p
-    fi
-  fi
-done
