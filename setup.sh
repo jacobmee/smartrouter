9bea92b3d0e249768382ebad4b73856575e64bef
Jacob <jacobmee@gmail.com>
2020-05-19 21:45:23 +0800

    Generating make config file with Nano

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+sudo timedatectl set-timezone Asia/Shanghai
+sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf
+
+cd ..
+git clone https://github.com/coolsnowwolf/lede
+get clone https://github.com/jacobmee/smartrouter.git
+cd lede/package
+git clone https://github.com/kenzok8/openwrt-packages
+#src-git small https://github.com/kenzok8/small
+cd ../..
+
+cd smartrouter/x86_64
+sh update.sh
