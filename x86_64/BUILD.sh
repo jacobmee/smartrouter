89b911df09202093f8c0b04df05305a0e3c0ba56
Jacob Mi <jacobmee@gmail.com>
2020-05-19 12:37:53 +0800

    Update for 2020.5.19

---
+++ x86_64/BUILD.sh
@@ -0,0 +1,38 @@
+# This build file is to setting up the files folder under ImageBuilder
+
+# Get where the current bash file located
+BASH_DIR=$(cd `dirname $0`; pwd)
+
+# Define the destination
+DEST=$BASH_DIR/..
+SRC=$BASH_DIR/src
+BUILD_DEST=$BASH_DIR/../../lede
+
+echo "# Clean and setup destination: "
+echo "# ImageBuilder: "$BUILD_DEST
+echo "# Target: "$DEST
+rm -rf $BUILD_DEST/files
+rm -rf $BUILD_DEST/bin
+#rm -rf $DEST/bin
+
+echo "# Copy neccessary files"
+mkdir $BUILD_DEST/files
+cp -r $SRC/* $BUILD_DEST/files/
+
+
+echo "Start to BUILD"
+cd $BUILD_DEST/
+
+make download -j5
+make -j5 V=s
+
+echo "# Copy the target file into this Bin files"
+cp $BUILD_DEST/bin/targets/x86/64/openwrt-x86-64-combined-squashfs.vmdk $DEST/bin/NetAccelerator_Lede_x86_64.vmdk
+
+echo "# Clean files"
+#make clean
+cd $DEST
+
+echo "##############################################"
+echo "#    DONE, Please check the BIN folder!      #"
+echo "##############################################"
---
+++ x86_64/update.sh
@@ -0,0 +1,18 @@
+BASH_DIR=$(cd `dirname $0`; pwd)
+
+# Define the destination
+DEST=$BASH_DIR
+SRC=$DEST/src/
+BUILD_DEST=$BASH_DIR/../../lede
+
+echo "Pull files from github"
+cd $BUILD_DEST
+git pull
+cd $BUILD_DEST/package/openwrt-packages
+git pull
+cd $BUILD_DEST
+
+echo "Start to update files from feed"
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+echo "******Finished******"
